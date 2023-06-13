76ac18934bb8d0c5c5c20bc3cc65f5d732319052
ToshaxarA <toshaxar@gmail.com>
2023-06-14 02:04:53 +0600

    Добавлены тесты

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.http import HttpRequest
+from django.shortcuts import render
+request = HttpRequest
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        
+    def test_index(self):
+        response = self.client.get(reverse("index-page"))
+        
+        self.assertIn('Главная страница', response.content.decode())
+
+    
+    def test_contacts(self):
+        response = self.client.get(reverse("contacts-page"))
+        
+        self.assertIn('Контакты', response.content.decode())
+
+    def test_about(self):
+        response = self.client.get(reverse("about-page"))
+        
+        self.assertIn('Страница о нас', response.content.decode())
+    
+
+    
